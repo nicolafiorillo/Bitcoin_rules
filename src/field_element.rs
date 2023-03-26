c88689f28392cd6b43c62d79993e74501df315d3
Nicola Fiorillo <nicola.fiorillo@gmail.com>
2023-03-26 18:42:27 +0200

    Finite Field division and exponentiation.

--- src/field_element.rs
+++ src/field_element.rs
@@ -10,6 +10,10 @@ pub struct FieldElement {
 impl FieldElement {
     // Create a new FieldElement
     pub fn new(num: Integer, prime: u32) -> FieldElement {
+        if prime < 2 {
+            panic!("invalid base: it must be equal or greater than 2");
+        }
+
         FieldElement {
             num,
             prime,
@@ -17,9 +21,11 @@ impl FieldElement {
         }
     }
 
-    // Exp operator
-    fn exp(self, exponent: u32) -> FieldElement {
-        let (_q, rem) = (self.num.pow(exponent)).div_rem_euc(Into::into(self.prime));
+    // Exp operator (Fermat's lIttle Theorem)
+    fn exp(self, exponent: i32) -> FieldElement {
+        let exp = exponent.rem_euclid(self.prime as i32 - 1) as u32;
+
+        let (_q, rem) = (self.num.pow(exp)).div_rem_euc(Into::into(self.prime));
         return FieldElement::new(rem, self.prime);
     }
 }
@@ -161,6 +167,32 @@ mod field_element_test {
     }
 
     #[test]
+    fn exercise8_1() {
+        let field1 = FieldElement::new(Integer::from(3), 31);
+        let field2 = FieldElement::new(Integer::from(24), 31);
+        let field3 = FieldElement::new(Integer::from(4), 31);
+
+        assert_eq!(field1 * field2.exp(-1), field3);
+    }
+
+    #[test]
+    fn exercise8_2() {
+        let field1 = FieldElement::new(Integer::from(17), 31);
+        let field2 = FieldElement::new(Integer::from(29), 31);
+
+        assert_eq!(field1.exp(-3), field2);
+    }
+
+    #[test]
+    fn exercise8_3() {
+        let field1 = FieldElement::new(Integer::from(4), 31);
+        let field2 = FieldElement::new(Integer::from(11), 31);
+        let field3 = FieldElement::new(Integer::from(13), 31);
+
+        assert_eq!(field1.exp(-4) * field2, field3);
+    }
+
+    #[test]
     fn exponentiationing_a_serie_7() {
         let v = a_serie(7);
         assert_eq!(v, a_vector_of_ones(7))
@@ -198,7 +230,7 @@ mod field_element_test {
         let mut v = vec![];
 
         for i in 1..p {
-            v.push(FieldElement::new(Integer::from(i), p).exp(p - 1));
+            v.push(FieldElement::new(Integer::from(i), p).exp(p as i32 - 1));
         }
 
         return v;

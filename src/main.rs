8f547f2ed7b555d3f28699eb6e67a7ee449b8cb7
Nicola Fiorillo <nicola.fiorillo@gmail.com>
2023-10-22 12:21:41 +0200

    Showing version.

--- src/main.rs
+++ src/main.rs
@@ -22,9 +22,13 @@ mod wallet;
 
 fn main() {
     env_logger::init();
-    println!("Bitcoin_rules!");
+    println!("Bitcoin_rules! (ver. {})", version());
     println!("A Bitcoin node written in Rust for educational purposes.");
     println!();
     println!("This is a work in progress: please do not use it in production.");
-    println!();
+}
+
+fn version() -> &'static str {
+    const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
+    VERSION.unwrap_or("unknown")
 }

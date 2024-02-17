39fea1b074737d366e6cb6ed331c3c19a9431048
Nicola Fiorillo <nicola.fiorillo@gmail.com>
2024-02-17 20:19:30 +0100

    Simple command line support.

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,21 @@ Entry point
 //     clippy::missing_panics_doc
 // )]
 
+pub mod cli;
+
+use crate::cli::command_line::{print_exit_help, run_command_line};
+
 fn main() {
     env_logger::init();
     println!("Bitcoin_rules! (ver. {})", version());
     println!("A Bitcoin node written in Rust for educational purposes.");
     println!();
     println!("This is a work in progress: please do not use it in production.");
+
+    print_exit_help();
+    run_command_line();
+
+    println!("Bye.");
 }
 
 fn version() -> &'static str {

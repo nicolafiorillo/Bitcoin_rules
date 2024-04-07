5929504f325d0dd3d89b4a92607e3e185740de96
Nicola Fiorillo <nicola.fiorillo@gmail.com>
2024-04-07 10:41:25 +0200

    Fix test.

--- br_lib/src/network/network_message.rs
+++ br_lib/src/network/network_message.rs
@@ -158,4 +158,19 @@ mod network_message_test {
         assert_eq!(network_message.command, VERACK_COMMAND);
         assert_eq!(network_message.payload, vec![0; 0]);
     }
+
+    #[test]
+    fn receive_verack() {
+        let received = [
+            11, 17, 9, 7, 118, 101, 114, 115, 105, 111, 110, 0, 0, 0, 0, 0, 102, 0, 0, 0, 76, 149, 14, 113, 128, 17, 1,
+            0, 9, 4, 0, 0, 0, 0, 0, 0, 233, 70, 12, 102, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            114, 80, 174, 11, 149, 73, 11, 34, 16, 47, 83, 97, 116, 111, 115, 104, 105, 58, 50, 54, 46, 48, 46, 48, 47,
+            47, 101, 39, 0, 1,
+        ];
+        let version_received = NetworkMessage::deserialize(&received, NetworkMagic::Testnet3).unwrap();
+        let message: Commands = version_received.clone().into();
+
+        assert!(matches!(message, Commands::Version { .. }));
+    }
 }
